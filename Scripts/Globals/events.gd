91c869b661ee73998508db587597a5dbe18a5c14
vikbro <vikso.todorov@gmail.com>
2025-08-02 15:54:03 +0300

    Added charcter Idle and Sit animation

---
+++ Scripts/Globals/events.gd
@@ -0,0 +1,16 @@
+extends Node
+# Global Event bus
+
+signal rotate_world(tween: Tween, from_right: bool)
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,36 @@
 extends Node2D
+class_name Player
 
+@export var rotation_speed: float
+@export var player_speed : float
+@onready var anim_tree: AnimationTree = $AnimationTree
+
+var state_machine
+var is_sitting := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	state_machine = anim_tree["parameters/playback"]
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("space"):
+		state_machine.travel("Dance")
+		is_sitting = false
+		
+	elif Input.is_action_pressed("down"):
+		state_machine.travel("Sit")
+		is_sitting = true
+		
+	elif Input.is_action_pressed("right"):
+		state_machine.travel("Walk")
+		is_sitting = false
+		# Rotate the world
+		Events.rotate_world.emit()
+	elif !is_sitting:  # Only go to Idle if we're not sitting
+		state_machine.travel("Idle")
+	
+	
 	pass
+	
